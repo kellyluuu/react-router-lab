6e3ee6e69ede3d1dad5353ff3ca8e68e5d450fe0
KELLY LUU <kellyluuu@gmail.com>
2022-05-21 23:01:16 -0500

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,10 +3,12 @@ import {Link} from "react-router-dom"
 export default function Nav (){
     return (
         <div className="navBar">
-            <Link to="/"><div>Home</div></Link>
-            <Link to="/about"><div>About</div></Link>
-
+            <button type="button" class="btn btn-outline-primary">
+            <Link to="/">Home</Link>
+            </button>
+            <button type="button" className="btn btn-outline-primary">
+            <Link to="/about">About</Link>
+            </button>
         </div>
-
     )
 }
